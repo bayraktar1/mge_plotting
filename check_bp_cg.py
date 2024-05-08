68bd1ea5be1e6baa0e2c4f2dd22e9dcfed5c6a14
bayraktar1 <Bayraktar.D@protonmail.com>
2024-05-08 16:03:44 +0200

    update

--- check_bp_cg.py
+++ check_bp_cg.py
@@ -4,44 +4,49 @@ from pathlib import Path
 import re
 
 
-def get_size_gc(input_file: str):
+def get_size_gc(input_file):
     """Function to get sequence size and GC content from fasta_file
 
     Parameters
     ----------
-    input_file : str
+    input_file : path object
         path/to/input/sequence.fasta
 
     Returns
     -------
-    list[str, int, float]
-        list containing file key, sequence length and GC%
+    list[str, int, float, str, str]
+        list containing file key, sequence length, GC%, accession, bin name
     """
-
+    filename = input_file.name
+    accession = str(filename).split('_')[0]
     FastaFile = open(input_file, "r")
     for rec in SeqIO.parse(FastaFile, "fasta"):
         name = rec.id
         seq = rec.seq
         seqLen = len(rec)
         G_C = gc_fraction(seq)
-        sequence_data = [input_file, name, seqLen, G_C]
+        sequence_data = [name, seqLen, G_C, accession, filename]
         return sequence_data
 
 
 def find_fasta_files(directory_path):
+    files_list = []
     root_dir = Path(directory_path)
     pattern = r'.*_bin_[0-9]{1,}\.fasta$'
     files = root_dir.rglob('*')
     for file in files:
         if re.match(pattern, file.name):
-            return file.resolve()
+            files_list.append(file.resolve())
+    return files_list
 
 
 if __name__ == "__main__":
-    fastafiles = find_fasta_files('.')
+    fastafiles = find_fasta_files('test_cg')
     data = []
     for path in fastafiles:
-        data.append(get_size_gc(str(path)))
+        data.append(get_size_gc(path))
 
-    with open('bp_cg.results', 'w') as file:
-        file.write(f'{data}\n')
+    with open('test_cg/bp_cg.results', 'w') as file:
+        file.write(f'Plasmid\tSeqLen\tCG\tAccession\tfile\n')
+        for row in data:
+            file.write(f'{'\t'.join(str(part) for part in row)}\n')
